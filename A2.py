2db4f02f508812ece466f74139fed4bdcce62e8e
Balapranav05 <vishalakshipranav@gmail.com>
2025-11-05 16:59:28 +0530

    Add files via upload

--- A2.py
+++ A2.py
@@ -1,13 +1,16 @@
-#Creating different variables
-x=5
-y="John"
-print(x)
-print(y)
+s1={2,3,1}
+s2={"b","a","c"}
+s3=list(zip(s1,s2))
+print(s3,"\n")
 
-codingal="codingal"
-print(codingal)
+list1=[10,20,30,40]
+list2=[100,200,300,400]
+for x,y in zip(list1, list2 [::-1]):
+    print(x,y)
 
-#User Input
-name=input("Enter your name: ")
+Stocks=["Reliance","Infosys","TCS"]
+Prices=[2175,1127,2750]
 
-print("\nHello", name,"\nWelcome to codingal")
+new_dict={Stocks: Prices for Stocks,Prices in zip(Stocks,Prices)}
+print("\n{}".format(new_dict))
+          
