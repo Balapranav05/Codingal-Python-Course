8c499cb6e5353814c40b6b4d5cbf45001e5b0a25
Balapranav05 <vishalakshipranav@gmail.com>
2025-11-14 17:19:44 +0530

    Add files via upload

---
+++ A2 Class Vehicle.py
@@ -0,0 +1,16 @@
+#Create class
+class Vehicle:
+
+    #Create init method
+    def __init__(self, max_speed, mileage):
+
+        #Bind the arguments
+        self.max_speed=max_speed
+        self.mileage=mileage
+
+#Object creation
+modelX=Vehicle(240,18)
+
+#Access the variables inside init method
+print("Model Max Speed:", modelX.max_speed)
+print("Model Mileage:", modelX.mileage)
