ca214be2311018cc295e93db9f8df1d1f3727a1c
Balapranav05 <vishalakshipranav@gmail.com>
2025-11-18 22:10:34 +0530

    Add files via upload

---
+++ A3 Super Penguin.py
@@ -0,0 +1,31 @@
+#Parent Class
+class Bird:
+
+    def __init__(self):
+        print("Bird is ready")
+
+    def whoisThis(self):
+        print("Bird")
+    
+    def swim(self):
+        print("Swim faster")
+
+#Child class
+class Penguin(Bird):
+
+    def __init__(self):
+        #Call super() function
+        super().__init__()
+        print("Penguin is ready")
+
+    def whoisThis(self):
+        print("Penguin")
+
+    def run(self):
+        print("Run faster")
+
+#Object creation
+peggy=Penguin()
+peggy.whoisThis()
+peggy.swim()
+peggy.run()
